0cc97680555f40511f85092f9cf13212a87bf5d5
Yug34 <vertilax1337@gmail.com>
2021-04-20 19:52:49 +0530

    Finished writing tests, linted test and index

--- index.js
+++ index.js
@@ -1,117 +1,113 @@
-var _ = require('lodash');
+const _ = require("lodash");
 
 function isValid(state, latest, transform) {
   for (const action of transform) {
-    if(action.state === "Move") {
+    if (action.state === "Move") {
       let temp = state[action.type][action.position];
-      state[action.type][action.position] = state[action.type][action.secondPosition];
+      state[action.type][action.position] =
+        state[action.type][action.secondPosition];
       state[action.type][action.secondPosition] = temp;
-      continue;
-    }
-    else if(action.state === "Insert") {
-      if(!state[action.type]) {
-        state[action.type] = []
+    } else if (action.state === "Insert") {
+      if (!state[action.type]) {
+        state[action.type] = [];
       }
 
       state[action.type].push(action.fileObj);
-      continue;
-    }
-    else if(action.state === "Delete") {
+    } else if (action.state === "Delete") {
       state[action.type].splice(action.position, 1);
     }
   }
 
-  console.log(_.isEqual(state,latest))
-  return _.isEqual(state,latest);
+  return _.isEqual(state, latest);
 }
 
 isValid(
   {
     video: [
-      { file: '1.mp4', customType: 'video' },
-      { file: '2.mp4', customType: 'video' },
-      { file: '3.mp4', customType: 'video' }
-    ]
+      { file: "1.mp4", customType: "video" },
+      { file: "2.mp4", customType: "video" },
+      { file: "3.mp4", customType: "video" },
+    ],
   },
   {
     video: [
-      { file: '2.mp4', customType: 'video' },
-      { file: '1.mp4', customType: 'video' }
+      { file: "2.mp4", customType: "video" },
+      { file: "1.mp4", customType: "video" },
     ],
-    image: [{ file: '1.png', customType: 'image' }]
+    image: [{ file: "1.png", customType: "image" }],
   },
   [
-    { state: 'Move', position: 0, secondPosition: 2, type: 'video' },
+    { state: "Move", position: 0, secondPosition: 2, type: "video" },
     {
-      state: 'Insert',
-      fileObj: { file: '1.png', customType: 'image' },
-      type: 'image'
+      state: "Insert",
+      fileObj: { file: "1.png", customType: "image" },
+      type: "image",
     },
     {
-      state: 'Delete',
+      state: "Delete",
       position: 0,
-      type: 'video'
-    }
+      type: "video",
+    },
   ]
-) // true
+); // true
 
 isValid(
   {},
   {
     video: [
-      { file: '1.mov', customType: 'video' },
-      { file: '2.mov', customType: 'video' }
+      { file: "1.mov", customType: "video" },
+      { file: "2.mov", customType: "video" },
     ],
     image: [
-      { file: '1.png', customType: 'image' },
-      { file: '2.png', customType: 'image' },
-      { file: '3.png', customType: 'image' }
-    ]
+      { file: "1.png", customType: "image" },
+      { file: "2.png", customType: "image" },
+      { file: "3.png", customType: "image" },
+    ],
   },
   [
     {
-      state: 'Insert',
-      fileObj: { file: '1.png', customType: 'image' },
-      type: 'image'
+      state: "Insert",
+      fileObj: { file: "1.png", customType: "image" },
+      type: "image",
     },
     {
-      state: 'Insert',
-      fileObj: { file: '1.mp3', customType: 'audio' },
-      type: 'audio'
+      state: "Insert",
+      fileObj: { file: "1.mp3", customType: "audio" },
+      type: "audio",
     },
     {
-      state: 'Insert',
-      fileObj: { file: '1.mov', customType: 'video' },
-      type: 'video'
+      state: "Insert",
+      fileObj: { file: "1.mov", customType: "video" },
+      type: "video",
     },
     {
-      state: 'Delete',
+      state: "Delete",
       position: 0,
-      type: 'video'
+      type: "video",
     },
     {
-      state: 'Insert',
-      fileObj: { file: '2.png', customType: 'image' },
-      type: 'image'
+      state: "Insert",
+      fileObj: { file: "2.png", customType: "image" },
+      type: "image",
     },
     {
-      state: 'Insert',
-      fileObj: { file: '3.png', customType: 'image' },
-      type: 'image'
+      state: "Insert",
+      fileObj: { file: "3.png", customType: "image" },
+      type: "image",
     },
     {
-      state: 'Insert',
-      fileObj: { file: '2.mov', customType: 'video' },
-      type: 'video'
+      state: "Insert",
+      fileObj: { file: "2.mov", customType: "video" },
+      type: "video",
     },
     {
-      state: 'Move',
+      state: "Move",
       position: 2,
       secondPosition: 1,
-      type: 'video'
-    }
+      type: "video",
+    },
   ]
-) // false
+); // false
 /***
  * Three reasons why
  * Audio not there
@@ -122,45 +118,45 @@ isValid(
 isValid(
   {
     video: [
-      { file: '1.mp4', customType: 'video' },
-      { file: '2.mp4', customType: 'video' },
-      { file: '3.mp4', customType: 'video' }
+      { file: "1.mp4", customType: "video" },
+      { file: "2.mp4", customType: "video" },
+      { file: "3.mp4", customType: "video" },
     ],
-    image: [{ file: '1.png', customType: 'image' }]
+    image: [{ file: "1.png", customType: "image" }],
   },
   {
     video: [
-      { file: '3.mp4', customType: 'video' },
-      { file: '1.mp4', customType: 'video' }
+      { file: "3.mp4", customType: "video" },
+      { file: "1.mp4", customType: "video" },
     ],
     image: [
-      { file: '1.png', customType: 'image' },
-      { file: '2.png', customType: 'image' }
-    ]
+      { file: "1.png", customType: "image" },
+      { file: "2.png", customType: "image" },
+    ],
   },
   [
-    { state: 'Move', position: 0, secondPosition: 2, type: 'video' },
+    { state: "Move", position: 0, secondPosition: 2, type: "video" },
     {
-      state: 'Insert',
-      fileObj: { file: '2.png', customType: 'image' },
-      type: 'image'
+      state: "Insert",
+      fileObj: { file: "2.png", customType: "image" },
+      type: "image",
     },
     {
-      state: 'Delete',
+      state: "Delete",
       position: 1,
-      type: 'video'
+      type: "video",
     },
     {
-      state: 'Insert',
-      fileObj: { file: '3.png', customType: 'image' },
-      type: 'image'
+      state: "Insert",
+      fileObj: { file: "3.png", customType: "image" },
+      type: "image",
     },
     {
-      state: 'Delete',
+      state: "Delete",
       position: 1,
-      type: 'image'
-    }
+      type: "image",
+    },
   ]
-) // false, wrong image deletion
+); // false, wrong image deletion
 
 module.exports = isValid;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,143 @@
 const isValid = require("./index");
 
-it('works', () => {
-    expect(false).toBe(false);
+it("first case", () => {
+  expect(
+    isValid(
+      {
+        video: [
+          { file: "1.mp4", customType: "video" },
+          { file: "2.mp4", customType: "video" },
+          { file: "3.mp4", customType: "video" },
+        ],
+      },
+      {
+        video: [
+          { file: "2.mp4", customType: "video" },
+          { file: "1.mp4", customType: "video" },
+        ],
+        image: [{ file: "1.png", customType: "image" }],
+      },
+      [
+        { state: "Move", position: 0, secondPosition: 2, type: "video" },
+        {
+          state: "Insert",
+          fileObj: { file: "1.png", customType: "image" },
+          type: "image",
+        },
+        {
+          state: "Delete",
+          position: 0,
+          type: "video",
+        },
+      ]
+    )
+  ).toBe(true);
 });
+
+it("second case", () => {
+  expect(
+    isValid(
+      {},
+      {
+        video: [
+          { file: "1.mov", customType: "video" },
+          { file: "2.mov", customType: "video" },
+        ],
+        image: [
+          { file: "1.png", customType: "image" },
+          { file: "2.png", customType: "image" },
+          { file: "3.png", customType: "image" },
+        ],
+      },
+      [
+        {
+          state: "Insert",
+          fileObj: { file: "1.png", customType: "image" },
+          type: "image",
+        },
+        {
+          state: "Insert",
+          fileObj: { file: "1.mp3", customType: "audio" },
+          type: "audio",
+        },
+        {
+          state: "Insert",
+          fileObj: { file: "1.mov", customType: "video" },
+          type: "video",
+        },
+        {
+          state: "Delete",
+          position: 0,
+          type: "video",
+        },
+        {
+          state: "Insert",
+          fileObj: { file: "2.png", customType: "image" },
+          type: "image",
+        },
+        {
+          state: "Insert",
+          fileObj: { file: "3.png", customType: "image" },
+          type: "image",
+        },
+        {
+          state: "Insert",
+          fileObj: { file: "2.mov", customType: "video" },
+          type: "video",
+        },
+        {
+          state: "Move",
+          position: 2,
+          secondPosition: 1,
+          type: "video",
+        },
+      ]
+    )
+  ).toBe(false);
+});
+
+it("third case", () => {
+  expect(isValid(
+      {
+        video: [
+          { file: "1.mp4", customType: "video" },
+          { file: "2.mp4", customType: "video" },
+          { file: "3.mp4", customType: "video" },
+        ],
+        image: [{ file: "1.png", customType: "image" }],
+      },
+      {
+        video: [
+          { file: "3.mp4", customType: "video" },
+          { file: "1.mp4", customType: "video" },
+        ],
+        image: [
+          { file: "1.png", customType: "image" },
+          { file: "2.png", customType: "image" },
+        ],
+      },
+      [
+        { state: "Move", position: 0, secondPosition: 2, type: "video" },
+        {
+          state: "Insert",
+          fileObj: { file: "2.png", customType: "image" },
+          type: "image",
+        },
+        {
+          state: "Delete",
+          position: 1,
+          type: "video",
+        },
+        {
+          state: "Insert",
+          fileObj: { file: "3.png", customType: "image" },
+          type: "image",
+        },
+        {
+          state: "Delete",
+          position: 1,
+          type: "image",
+        },
+      ]
+  )).toBe(false);
+})
